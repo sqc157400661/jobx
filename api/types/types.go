004b6072aa7fa38ec6cbc57ccadd37afa60722a2
sqc <sqc157400661@163.com>
2025-04-07 22:28:30 +0800

    modify job api

--- api/types/types.go
+++ api/types/types.go
@@ -1,35 +1,5 @@
 package types
 
-type CreateOptions struct {
-	Name  string                 `json:"name"`
-	Desc  string                 `json:"desc"`
-	Pause bool                   `json:"pause"`
-	Input map[string]interface{} `json:"input"`
-	Env   map[string]interface{} `json:"env"`
-}
-
-type CreateJober struct {
-	Owner  string `json:"owner"`
-	Tenant string `json:"tenant"`
-	BizId  string `json:"biz_id"`
-	CreateOptions
-	ChildJobs []*CreateJober
-	Pipelines []*CreatePipeline
-}
-
-type CreatePipeline struct {
-	Action string `json:"action"`
-	Retry  int    `json:"retry"`
-	CreateOptions
-}
-
-type CommonParam struct {
-	Timestamp string `json:"timestamp"`
-	UID       string `json:"UID"`
-	Action    string `json:"Action,omitempty"`
-	Method    string `json:"method"`
-}
-
 type Pagination struct {
 	PageIndex int `form:"pageIndex" json:"pageIndex" query:"pageIndex"`
 	PageSize  int `form:"pageSize"  json:"pageSize"  query:"pageIndex"`
